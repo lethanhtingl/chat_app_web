c062b24e98adb592a46755d6324948e0a075bb35
lethanhtingl <ltt.lethanhtin@gmail.com>
2023-04-02 14:18:01 +0700

    env-file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("API is Running")
+    res.send("API is Running Successfull")
 })
 
 app.get('/api/chat', (req, res) => {
     res.send(chats);
 })
 
-app.listen(5000, console.log("Server start on Port 5000"));
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find(c=>c._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server start on Port ${PORT}"`));
